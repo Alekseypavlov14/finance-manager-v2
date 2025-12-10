5d0ad02db3a917d684ae9ca960e90ed24debdd17
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-12-10 20:21:49 +0200

    added transaction form in column for received and lost

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,12 @@
+import { defaultTransactionFormData, TransactionForm } from '@/features/transaction-form'
 import { AppBurgerButton } from '@/widgets/AppBurgerButton'
 import { AppSidebar } from '@/widgets/AppSidebar'
 import { Container } from '@/shared/components/Container'
 import { Wrapper } from '@/shared/components/Wrapper'
+import { Palette } from '@/shared/components/Palette'
 import { Header } from '@/shared/components/Header'
 import { Page } from '@/shared/components/Page'
 import { Main } from '@/shared/components/Main'
-import { defaultTransactionFormData, TransactionForm } from '@/features/transaction-form'
 
 export function HomePage() {
   return (
@@ -21,9 +22,11 @@ export function HomePage() {
           <Container>
             Home page
 
-            <TransactionForm 
-              initialTransactionData={defaultTransactionFormData}
-            />
+            <Palette block round>
+              <TransactionForm 
+                initialTransactionData={defaultTransactionFormData}
+              />
+            </Palette>
           </Container>
         </Main>
       </Wrapper>
--- src/shared/components/Palette/Palette.tsx
+++ src/shared/components/Palette/Palette.tsx
@@ -2,12 +2,23 @@ import type { ComponentProps } from 'react'
 import styles from './Palette.module.css'
 import clsx from 'clsx'
 
-interface PaletteProps extends ComponentProps<'div'> {}
+interface PaletteProps extends ComponentProps<'div'> {
+  block?: boolean
+  round?: boolean
+}
 
-export function Palette({ className, children, ...props }: PaletteProps) {
+export function Palette({ 
+  className, 
+  children, 
+
+  block,
+  round,
+
+  ...props 
+}: PaletteProps) {
   return (
     <div 
-      className={clsx(styles.Palette, className)}
+      className={clsx(styles.Palette, block && styles.Block, round && styles.Round, className)}
       {...props}
     >
       {children}
