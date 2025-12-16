e1a18817ee286c5553ab04d669dd5476a3f7d90c
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-12-16 23:11:22 +0200

    fixed charts util

--- src/features/charts/utils/get-peaks-from-sample.ts
+++ src/features/charts/utils/get-peaks-from-sample.ts
@@ -1,7 +1,7 @@
 import type { DataDomain } from '../types/data-domain'
 
 export function getPeaksFromSample(data: number[]): DataDomain {
-  const min = Math.min(...data, 0)
-  const max = Math.max(...data, 0)
+  const min = Math.min(...data) || 0
+  const max = Math.max(...data) || 0
   return [ min, max ]
 }
